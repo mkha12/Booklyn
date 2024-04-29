108fc9f79bc5bb2d5f859ba0665222ee9cbcfa6a
mariia <mkhavantseva@gmail.com>
2024-04-29 12:19:00 +0300

    change auth controller (delete sing in/ up add google and apple

--- Booklyn/AuthViewController.swift
+++ Booklyn/AuthViewController.swift
@@ -11,14 +11,18 @@ final class AuthViewController: UIViewController {
     
     private var greeting: UILabel!
     private var greetingDescrip: UILabel!
-    private var signInButton: UIButton!
-    private var signUpButton: UIButton!
+    private var signAppleButton: UIButton!
+    private var signGoogleButton: UIButton!
     private var privaceLabelButton: UIButton!
+    private var leftSeparatorLine: UIView!
+    private var rightSeparatorLine: UIView!
+    private var orLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
         setupUI()
+        setupOrSeparator()
         setupConstraint()
     }
     
@@ -41,23 +45,29 @@ final class AuthViewController: UIViewController {
         greetingDescrip.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(greetingDescrip)
         
-        signInButton = UIButton()
-        signInButton.setTitle("Sign In", for: .normal)
-        signInButton.setTitleColor(.white, for: .normal)
-        signInButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .medium)
-        signInButton.backgroundColor = .brown
-        signInButton.layer.cornerRadius = 15
-        signInButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(signInButton)
+        signAppleButton = UIButton()
+        signAppleButton.setImage(UIImage(systemName: "applelogo"), for: .normal)
+        signAppleButton.setTitle("Continue with Apple", for: .normal)
+        signAppleButton.setTitleColor(.black, for: .normal)
+        signAppleButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .medium)
+        signAppleButton.backgroundColor = .clear
+        signAppleButton.layer.borderWidth = 1
+        signAppleButton.layer.borderColor = UIColor.gray.cgColor
+        signAppleButton.layer.cornerRadius = 15
+        signAppleButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(signAppleButton)
         
-        signUpButton = UIButton()
-        signUpButton.setTitle("Sing Up", for: .normal)
-        signUpButton.setTitleColor(.white, for: .normal)
-        signUpButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .medium)
-        signUpButton.backgroundColor = .brown
-        signUpButton.layer.cornerRadius = 15
-        signUpButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(signUpButton)
+        signGoogleButton = UIButton()
+        signGoogleButton.setImage(UIImage(systemName: "googlelogo"), for: .normal)
+        signGoogleButton.setTitle("Continue with Google", for: .normal)
+        signGoogleButton.setTitleColor(.black, for: .normal)
+        signGoogleButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .medium)
+        signGoogleButton.layer.borderWidth = 1
+        signGoogleButton.layer.borderColor = UIColor.gray.cgColor
+        signGoogleButton.backgroundColor = .clear
+        signGoogleButton.layer.cornerRadius = 15
+        signGoogleButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(signGoogleButton)
         
         privaceLabelButton = UIButton()
         privaceLabelButton.setTitle("Privacy policy", for: .normal)
@@ -69,7 +79,32 @@ final class AuthViewController: UIViewController {
         view.addSubview(privaceLabelButton)
     }
     
+    private func setupOrSeparator() {
+        
+        orLabel = UILabel()
+        orLabel.text = "or"
+        orLabel.textColor = .gray
+        orLabel.font = UIFont.systemFont(ofSize: 14)
+        orLabel.textAlignment = .center
+        orLabel.backgroundColor = .white
+        orLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(orLabel)
+        
+        leftSeparatorLine = UIView()
+        leftSeparatorLine.backgroundColor = .lightGray
+        leftSeparatorLine.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(leftSeparatorLine)
+        
+        rightSeparatorLine = UIView()
+        rightSeparatorLine.backgroundColor = .lightGray
+        rightSeparatorLine.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(rightSeparatorLine)
+        
+        
+    }
+    
     private func setupConstraint() {
+        
         NSLayoutConstraint.activate([
             
             greeting.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor,constant: 150),
@@ -80,17 +115,33 @@ final class AuthViewController: UIViewController {
             greetingDescrip.topAnchor.constraint(equalTo:greeting.bottomAnchor,constant: 10),
             greetingDescrip.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
-            signInButton.topAnchor.constraint(equalTo: greetingDescrip.bottomAnchor,constant: 80),
-            signInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            signInButton.widthAnchor.constraint(equalToConstant: 200),
-            signInButton.heightAnchor.constraint(equalToConstant: 50),
+            signAppleButton.topAnchor.constraint(equalTo: greetingDescrip.bottomAnchor,constant: 80),
+            signAppleButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            //signAppleButton.widthAnchor.constraint(equalToConstant: 200),
+            signAppleButton.heightAnchor.constraint(equalToConstant: 50),
+            signAppleButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            signAppleButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             
-            signUpButton.topAnchor.constraint(equalTo: signInButton.bottomAnchor,constant: 10),
-            signUpButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            signUpButton.widthAnchor.constraint(equalToConstant: 200),
-            signUpButton.heightAnchor.constraint(equalToConstant: 50),
+            signGoogleButton.topAnchor.constraint(equalTo: signAppleButton.bottomAnchor,constant: 10),
+            signGoogleButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signGoogleButton.heightAnchor.constraint(equalToConstant: 50),
+            signGoogleButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            signGoogleButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             
-            //privaceLabelButton.topAnchor.constraint(equalTo: signUpButton.bottomAnchor,constant: 100),
+            leftSeparatorLine.centerYAnchor.constraint(equalTo: orLabel.centerYAnchor),
+            leftSeparatorLine.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            leftSeparatorLine.trailingAnchor.constraint(equalTo: orLabel.leadingAnchor, constant: -10),
+            leftSeparatorLine.heightAnchor.constraint(equalToConstant: 1),
+            rightSeparatorLine.centerYAnchor.constraint(equalTo: orLabel.centerYAnchor),
+            rightSeparatorLine.leadingAnchor.constraint(equalTo: orLabel.trailingAnchor, constant: 10),
+            rightSeparatorLine.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            rightSeparatorLine.heightAnchor.constraint(equalToConstant: 1),
+        
+            orLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            orLabel.topAnchor.constraint(equalTo: signGoogleButton.bottomAnchor, constant: 20),
+            orLabel.widthAnchor.constraint(equalToConstant: 30),
+            orLabel.heightAnchor.constraint(equalToConstant: 20),
+            
             privaceLabelButton.bottomAnchor.constraint(equalTo:view.safeAreaLayoutGuide.bottomAnchor,constant: 20),
             privaceLabelButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             privaceLabelButton.widthAnchor.constraint(equalToConstant: 200),
@@ -98,4 +149,6 @@ final class AuthViewController: UIViewController {
             
         ])
     }
-}
+
+   }
+
